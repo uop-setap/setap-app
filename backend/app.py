74365080526c0fcd13bd74bff137198d93a0c659
Sarah Isaacs <49172425+srhiscs@users.noreply.github.com>
2024-03-19 19:43:02 +0000

    Added Firebase SDK

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,12 @@ import os
 import openai
 from flask_cors import CORS
 import requests
+from firebase_admin import credentials, firestore, auth, initialize_app
+
 app = Flask(__name__)
+
+# initialize the Firebase SDK
+default_app = firebase_admin.initialize_app()
 
 @app.route("/")
 def root():
